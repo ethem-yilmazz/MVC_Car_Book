9a920c60b030118193d3e2c31aa82549a8ce84a8
ethem yilmaz <ethem_yilmaz95@icloud.com>
2024-04-17 20:06:39 +0300

    Add project files.

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Queries/PricingQueries/GetPricingByIdQueries.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.Mediator.Results.PricingResults;
+
+namespace UdemyCarBook.Application.Features.Mediator.Queries.PricingQueries
+{
+    public class GetPricingByIdQueries :IRequest<GetPricingByIdQueryResult>
+    {
+        public int Id { get; set; }
+
+        public GetPricingByIdQueries(int id)
+        {
+            Id = id;
+        }
+    }
+}
