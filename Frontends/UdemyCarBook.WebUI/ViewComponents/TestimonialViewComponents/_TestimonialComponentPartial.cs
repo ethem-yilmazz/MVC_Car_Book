111ddc5bdc79c3540952acf2ad96cb48f38bd4dc
ethem yilmaz <ethem_yilmaz95@icloud.com>
2024-04-20 00:31:01 +0300

    #

--- Frontends/UdemyCarBook.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
+++ Frontends/UdemyCarBook.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using UdemyCarBook.Dto.TestimonialDtos;
 
 namespace UdemyCarBook.WebUI.ViewComponents.TestimonialViewComponents
 {
@@ -15,11 +16,11 @@ namespace UdemyCarBook.WebUI.ViewComponents.TestimonialViewComponents
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7086/api/Abouts");
+            var responseMessage = await client.GetAsync("https://localhost:7086/api/Testimonials");
             if(responseMessage.IsSuccessStatusCode)
             {
                 var jsonData =await responseMessage.Content.ReadAsStringAsync();
-                var values =JsonConvert.DeserializeObject<List<>>(jsonData);
+                var values =JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
                 return View(values);
             }
             return View();
